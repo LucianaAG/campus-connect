bfe410497cc20f99f3c57e9f08dc67da53839453
LucyAG <lucygauto35@gmail.com>
2025-11-11 04:52:06 -0300

    fix: errors in description form

--- controllers/inscription_controller.js
+++ controllers/inscription_controller.js
@@ -27,6 +27,7 @@ module.exports.render_inscription_register_form = async (request, response) => {
     }
 };
 
+// ------------------ Controlador de registro ------------------
 module.exports.inscription_register_form = async (request, response) => {
     const subject_id = request.body.subject_id;
     const student_id = request.body.student_id;
@@ -46,5 +47,95 @@ module.exports.inscription_register_form = async (request, response) => {
         console.log('Error al registrar la inscripción', error);
         request.flash('error_msg', 'Ocurrió un error al registrar la inscripción')
         response.redirect('/inscription/register');
+    }
+};
+
+// ------------------ Controlador de listado ------------------
+
+module.exports.inscriptions_list = async (request, response) => {
+
+        try {
+        const inscriptions = await Inscription.findAll({raw: true});
+        response.render('inscription/list', {inscriptions});
+    } catch (error) {
+        console.log('Error al listar los registros', error);
+        request.flash('error_msg', 'Ocurrió un error al listar los registros');
+        response.redirect('/home');
+    }
+};
+
+module.exports.render_inscription_edit_form = async (request, response) => {
+    try {
+        const subjects = await Subject.findAll({
+            order: [['name', 'ASC']],
+            raw: true
+        });
+
+        const students = await Student.findAll({
+            order: [['name', 'ASC']],
+            raw: true
+        });
+
+        response.render('inscription/edit', {subjects, students});
+    } catch (error) {
+        console.error('error al cargar materias y estudiantes:', error.message);
+        request.flash('error_msg', 'no se pudieron cargar las materias y estudiantes');
+        response.redirect('/');
+    }
+};
+
+module.exports.inscription_edit_form = async (request, response) => {
+    const errors = validationResult(request);
+    const inscription_id = request.params.inscription_id;
+
+    if (!errors.isEmpty()) {
+        response.redirect('/inscription/edit',
+            {
+                errors: errors.array(),
+                inscription: {
+                    student_id: request.body.student_id,
+                    subject_id: request.body.subject_id,
+                    inscription_date: request.body.inscription_date
+                }
+            }
+        );
+    }
+
+    try {
+        const student_id = request.body.student_id
+        const subject_id = request.body.subject_id
+        const inscription_date = request.body.inscription_date
+
+        const update_data = {student_id, subject_id, inscription_date};
+        await Inscription.update(update_data, {where: {inscription_id}});
+        request.flash('succes_msg', 'El registro se ha actualizado con exito');
+        response.redirect('/inscription/list');
+    } catch (error) {
+        console.error('Error al actualizar el registro', error.message);
+
+        if (error.parent) {
+            console.error('Detalle SQL', error.parent.sqlMessage);
+        }
+        request.flash('error_msg', 'Ocurrió un erorr al actualizar el registro');
+        response.redirect('/inscription/list');
+    }
+};
+
+// ------------------ Controlador de eliminación ------------------
+module.exports.delete_inscription = async (request, response) => {
+    const inscription_id = request.params.inscription_id;
+
+    try {
+        await Inscription.destroy({where: {inscription_id}});
+        request.flash('success_msg', 'El registro se ha eliminado con exito');
+        response.redirect('(student/list');
+    } catch (error) {
+        console.error('errro al eliminar el registro', error.message);
+
+        if (error.parent) {
+            console.error('Detalle SQL', error.parent.sqlMessage);
+        }
+        request.flash('error_msg', 'Ocurrió un error al eliminar el registro');
+        response.redirect('/student/list');
     }
 };
--- routes/inscription_routes.js
+++ routes/inscription_routes.js
@@ -11,4 +11,14 @@ router.get('/', (req, res) => {
 router.get('/register', ensure_authenticated, inscription_controllers.render_inscription_register_form);
 router.post('/register', ensure_authenticated, inscription_controllers.inscription_register_form);
 
+// ------------------ Rutas de listado ------------------
+router.get('/list', ensure_authenticated, inscription_controllers.inscriptions_list);
+
+// ------------------ Rutas de registro ------------------
+router.get('/edit/:inscription_id', ensure_authenticated, inscription_controllers.render_inscription_edit_form);
+router.post('/edit/:inscription_id', ensure_authenticated, inscription_controllers.inscription_edit_form);
+
+// ------------------ Rutas de elimianción ------------------
+router.post('/delete/:inscription_id', ensure_authenticated, inscription_controllers.delete_inscription);
+
 module.exports = router;
